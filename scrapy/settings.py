5cf4eeaa9a254bfee597415a8bf74ad5180257dd
Wei <zhengwei8618@gmail.com>
2019-10-05 18:57:26 +0800

    utf8 encoding

--- scrapy/settings.py
+++ scrapy/settings.py
@@ -8,7 +8,7 @@ chrome_options = Options()
 # chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_path = '/home/centos/football/chromedriver'
+chrome_path = '/home/centos/PythonApp/chromedriver'
 
 def get_soup(url):
     display = Display(visible=0, size=(800, 600))
