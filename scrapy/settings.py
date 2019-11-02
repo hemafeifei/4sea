f14d06b3ad9736e5603aa45b6408e7e192e9fa2f
Wei Zheng <wei@WeideMacBook-Pro.local>
2019-11-02 18:06:18 +0800

    modi setting

--- scrapy/settings.py
+++ scrapy/settings.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 import sys
 
 chrome_options = Options()
-# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
