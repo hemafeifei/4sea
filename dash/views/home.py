e27f1b97b28e29f5b813f7ae5bc045db5d8bcda6
Wei Zheng <wei@WeideMacBook-Pro.local>
2019-11-08 10:34:08 +0800

    v0.35

--- dash/views/home.py
+++ dash/views/home.py
@@ -1,19 +1,6 @@
-import warnings
-# Dash configuration
-import dash
-import dash_core_components as dcc
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
 import dash_html_components as html
-from dash.dependencies import Input, Output, State
-import pandas as pd
-import dash_dangerously_set_inner_html
-from server import app
-
-# FI_IMAGE = 'https://cdn.earlytorise.com/wp-content/uploads/2018/02/stockmarket.jpg'
-# FT_IMAGE = 'http://www.freelunch.co.in/wp-content/uploads/2018/07/machine-learning-in-sports-730x410.png'
-# SEC_IMAGE = 'https://www.hsbc.com.hk/content/dam/hsbc/hk/images/investments/16-9/stocks-trading-graph-21364.jpg/jcr:content/renditions/cq5dam.web.300.1000.jpeg'
-# TMP_IMAGE = 'https://catanacapital.com/wp-content/uploads/2019/03/Mathematical_Concept-780x438.jpg'
-# TMP_IMAGE2 = 'http://img5.imgtn.bdimg.com/it/u=1950189138,323868647&fm=26&gp=0.jpg'
-# ABT_IMAGE = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTLanMv0-Ovx-Zlftqdi5lV_oYJ9OYuNmiG3hbVKny5JLWuWgVP'
 
 FI_IMAGE = 'https://i.loli.net/2019/11/04/7mPCGdKe2XJfEoA.jpg '
 FT_IMAGE = 'https://i.loli.net/2019/11/04/p3A2nbrKfyLD1qX.jpg'
@@ -40,7 +27,7 @@ layout = html.Div([
                                style={'color': 'turquoise'}),
 
                         html.Div([
-                            html.A(" 全球指数 ",  href='/wealth', className='tag'),
+                            html.A(" 全球指数 ", href='/wealth', className='tag'),
                             "|",
                             html.A(" 沪深市场 ", href='/wealth/sec', className='tag'),
                             "|",
@@ -49,9 +36,9 @@ layout = html.Div([
                             # html.A(" 外汇 ", ),
                         ], className='row'),
                         html.Hr(),
-                        html.H5("The Beast stumbled in the dark for it could no longer see the path. It started to "
-                                "fracture and weaken, trying to reshape itself into the form of metal. Even the witches "
-                                "would no longer lay eyes upon it, for it had become hideous and twisted.",
+                        html.H5("巴菲特建议普通人购买的SP500指数，在过去40年期间，年化收益可达7.5%左右。在中国融入全球化近20年后的今天，"
+                                "证券市场显然是值得关注的地方。当然中国很多好的企业在境外上市，因此投资中国证券市场需要关注全球市场。"
+                                "如果你还懂一点量化策略，可以预期收益10%-15%",
                                 className='content')
                     ], className='card-content')
                 ], className='card large round')
@@ -69,13 +56,13 @@ layout = html.Div([
                             "|",
                             html.A(" 历史回查 ", href='/football/his', className='tag'),
                             "|",
-                            html.A(" 赛果预测 ", href='/football/ml',  className='tag'),
+                            html.A(" 赛果预测 ", href='/football/ml', className='tag'),
 
                         ], className='row'),
                         html.Hr(),
-                        html.H5("The Beast stumbled in the dark for it could no longer see the path. It started to "
-                                "fracture and weaken, trying to reshape itself into the form of metal. Even the witches "
-                                "would no longer lay eyes upon it, for it had become hideous and twisted.",
+                        html.H5("足球的魅力无与伦比，一切皆有可能，但比赛的结果并非完全不可预测。"
+                                "我们持续追踪10个赛季的五大联赛，发现有一部分比赛是有明显的赔率波动趋势。根据这一趋势，使用机器学习"
+                                "进行建模并预测，持续增加的数据让我们的模型不断进步，上赛季预测准确率超过67%。",
                                 className='content')
                     ], className='card-content')
                 ], className='card large round')
@@ -95,9 +82,9 @@ layout = html.Div([
 
                         ], className='row'),
                         html.Hr(),
-                        html.H5("The Beast stumbled in the dark for it could no longer see the path. It started to "
-                                "fracture and weaken, trying to reshape itself into the form of metal. Even the witches "
-                                "would no longer lay eyes upon it, for it had become hideous and twisted.",
+                        html.H5("追踪舆论热点与财经温度。追踪财经市场与情绪相关的Sentiment分析。"
+                                "从微博、谷歌反馈的数据来看：中国人都在追娱乐新闻，德国人都在关注球赛，美国人在搜 Taylor Swift。"
+                                "自这届美国总统上任，舆论认为世界进入了逆全球化阶段，其实这些年各个地区都是各玩各的吧....",
                                 className='content')
                     ], className='card-content')
                 ], className='card large round')
@@ -110,9 +97,7 @@ layout = html.Div([
                     html.Div([
                         html.A("Tools", className="title is-3 ", style={'color': 'turquoise'}),
                         html.Hr(),
-                        html.H5("The Beast stumbled in the dark for it could no longer see the path. It started to "
-                                "fracture and weaken, trying to reshape itself into the form of metal. Even the witches "
-                                "would no longer lay eyes upon it, for it had become hideous and twisted.",
+                        html.H5("基金定投、财富规划、保险计算等实用工具。",
                                 className='content')
                     ], className='card-content')
                 ], className='card large round')
@@ -125,9 +110,7 @@ layout = html.Div([
                     html.Div([
                         html.A("Learning", className="title is-3", style={'color': 'turquoise'}),
                         html.Hr(),
-                        html.H5("The Beast stumbled in the dark for it could no longer see the path. It started to "
-                                "fracture and weaken, trying to reshape itself into the form of metal. Even the witches "
-                                "would no longer lay eyes upon it, for it had become hideous and twisted.",
+                        html.H5("趣味实验室。一切新奇的想法都可以进行测试，比如已经蓄势待发近N年的：地图炮、转会市场网...",
                                 className='content')
                     ], className='card-content')
                 ], className='card large round')
@@ -140,13 +123,13 @@ layout = html.Div([
                     html.Div([
                         html.A("About", className="title is-3", href='/about', style={'color': 'turquoise'}),
                         html.Hr(),
-                        html.H5("The Beast stumbled in the dark for it could no longer see the path. It started to "
-                                "fracture and weaken, trying to reshape itself into the form of metal. Even the witches "
-                                "would no longer lay eyes upon it, for it had become hideous and twisted.",
+                        html.H5("关于我们。"
+                                "你不是一个人在战斗。"
+                                "Good Luck! Viel Spaß!",
                                 className='content')
                     ], className='card-content')
                 ], className='card large round')
             ], className='column is-one-third'),
         ], className='row columns is-multiline')
     ], className='section'),
-], className='container',)
+], className='container', )
